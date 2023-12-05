9f18c2fbcb6f2a25c0ee28b721176dbf663e1bdd
Yoann Kergall <yoann.kergall@gmail.com>
2023-12-05 22:56:34 +0100

    feat(chap-04): js-packages

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,10 @@
  */
 import './styles/app.css'
 import alienGreeting from './lib/alien-greeting.js';
+import JSConfetti from 'js-confetti'
+
+const jsConfetti = new JSConfetti()
+jsConfetti.addConfetti()
 
 alienGreeting('Give us all your candy!', false);
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
