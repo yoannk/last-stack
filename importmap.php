a98dd30a0fb394ab8268e1c62af699c268f37b99
Yoann Kergall <yoann.kergall@gmail.com>
2023-12-18 23:30:46 +0100

    feat(chap-08): ux-packages

--- importmap.php
+++ importmap.php
@@ -27,4 +27,11 @@ return [
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
+    'tom-select' => [
+        'version' => '2.3.1',
+    ],
+    'tom-select/dist/css/tom-select.default.css' => [
+        'version' => '2.3.1',
+        'type' => 'css',
+    ],
 ];
