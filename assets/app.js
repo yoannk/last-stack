f5fe1b61f7a6270e6550fc02c3ed776d26fc0c55
Yoann Kergall <yoann.kergall@gmail.com>
2023-12-18 21:25:59 +0100

    feat(chap-07): stimulus

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,6 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+import './bootstrap.js';
 import './styles/app.css'
 import alienGreeting from './lib/alien-greeting.js';
-import JSConfetti from 'js-confetti'
-
-const jsConfetti = new JSConfetti()
-jsConfetti.addConfetti()
 
 alienGreeting('Give us all your candy!', false);
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
